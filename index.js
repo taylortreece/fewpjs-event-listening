e989426bcf6103d62480a5a52c4bb4d0098474a6
taylortreece <tayloratreece@gmail.com>
2021-07-08 16:20:32 -0500

    commit tree 2b80ef36e5e11b90df36ce6ebcf3063df9978c32

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    // defining the node that will be listening for the event
+    const input = document.getElementById('input');
+
+    // defining the event and the response to the event
+    input.addEventListener('click', function(event) {
+      alert('I was clicked!');
+    }); 
 }
